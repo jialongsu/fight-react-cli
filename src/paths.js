b26b42fb2755a7efebc5d945be57b73e1b67025c
jialong.su <jialong.su@aliyun.com>
2021-11-15 16:40:26 +0800

    init
    init

--- src/paths.js
+++ src/paths.js
@@ -2,7 +2,7 @@ const path = require('path');
 const commanderDirname = process.cwd(); // 终端输入命令的路径
 const projectDirname = path.resolve(__dirname, '../');
 const packagePath = `${projectDirname}/packages`;
-const templateWebpackPath = `${packagePath}/fcc-template-wbpack`;
+const templateWebpackPath = `${packagePath}/fcc-template-webpack`;
 
 module.exports = {
   commanderDirname,
